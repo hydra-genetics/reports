80d6d475dc9dda90cd045d706a83a839383c753e
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-09-04 09:37:50 +0200

    Add test case from real report

--- .tests/unit/test_merge_cnv_json.py
+++ .tests/unit/test_merge_cnv_json.py
@@ -92,6 +92,68 @@ class TestMergeCnvJson(unittest.TestCase):
                 expected_names=["cnvkit", "gatk"],
                 expected_filter={"cnvkit": [False, True, False], "gatk": [True]},
             ),
+            TestCase(
+                name="example from real report",
+                input={
+                    "cnvkit": [
+                        CNV(
+                            caller="cnvkit",
+                            chromosome="chr16",
+                            genes=["gene1"],
+                            start=132_309,
+                            length=3_598_391,
+                            type="DEL",
+                            copy_number=1.41,
+                            baf=0.5,
+                        ),
+                        CNV(
+                            caller="cnvkit",
+                            chromosome="chr16",
+                            genes=["gene2"],
+                            start=3_731_199,
+                            length=31_683_311,
+                            type="COPY_NORMAL",
+                            copy_number=1.85,
+                            baf=0.5,
+                        ),
+                        CNV(
+                            caller="cnvkit",
+                            chromosome="chr16",
+                            genes=["gene3"],
+                            start=66_483_605,
+                            length=4_031_951,
+                            type="DEL",
+                            copy_number=1.28,
+                            baf=0.5,
+                        ),
+                        CNV(
+                            caller="cnvkit",
+                            chromosome="chr16",
+                            genes=["gene4"],
+                            start=87_570_960,
+                            length=2_524_518,
+                            type="DEL",
+                            copy_number=1.20,
+                            baf=0.5,
+                        ),
+                    ],
+                    "gatk": [
+                        CNV(
+                            caller="gatk",
+                            chromosome="chr16",
+                            genes=["gene1", "gene2", "gene3", "gene4"],
+                            start=132_060,
+                            length=89_963_668,
+                            type="COPY_NORMAL",
+                            copy_number=0.99,
+                            baf=0.5,
+                        )
+                    ],
+                },
+                expected_names=["cnvkit", "gatk"],
+                expected_lens={"cnvkit": 4, "gatk": 1},
+                expected_filter={"cnvkit": [True, False, True, True], "gatk": [True]},
+            ),
         ]
 
         for case in testcases:
@@ -106,10 +168,7 @@ class TestMergeCnvJson(unittest.TestCase):
                 filtered_cnvs=filtered_cnvs,
             )
 
-            assert len(m) == len(case.expected_names)
-
-            for caller, expected_caller in zip(m.keys(), case.expected_names):
-                assert caller == expected_caller
+            assert list(m.keys()) == case.expected_names
 
             for caller, cnvs in m.items():
                 assert len(cnvs) == case.expected_lens[caller]
