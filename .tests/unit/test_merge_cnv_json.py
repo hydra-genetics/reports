0a468a366d3c69a9a2882c7ded3af0e2809550f2
Niklas Mähler <niklas.mahler@gmail.com>
2025-04-01 16:19:07 +0200

    Add failing unit test

--- .tests/unit/test_merge_cnv_json.py
+++ .tests/unit/test_merge_cnv_json.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 import os
 import sys
-from typing import Dict, List
+from typing import Any, Dict, List, Tuple
 import unittest
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 SCRIPT_DIR = os.path.abspath(os.path.join(TEST_DIR, "../../workflow/scripts"))
 sys.path.insert(0, SCRIPT_DIR)
 
-from merge_cnv_json import CNV, filter_chr_cnvs  # noqa
+from merge_cnv_json import CNV, filter_chr_cnvs, merge_cnv_dicts  # noqa
 
 
 class TestMergeCnvJson(unittest.TestCase):
@@ -174,3 +174,143 @@ class TestMergeCnvJson(unittest.TestCase):
                 assert len(cnvs) == case.expected_lens[caller]
                 for cnv, expected_filter in zip(cnvs, case.expected_filter[caller]):
                     assert cnv["passed_filter"] == expected_filter
+
+    def test_merge_cnv_dicts(self):
+        @dataclass
+        class TestCase:
+            chromosomes: List[Tuple[str, int]]
+            callers: List[Dict[str, str]]
+            unfiltered_cnvs: List[List[CNV]]
+            filtered_cnvs: List[List[CNV]]
+            expected_cnvs: Dict[str, Any]
+
+        testcases = [
+            TestCase(
+                chromosomes=[("chr1", 1000)],
+                callers=[
+                    {
+                        "caller": "cnvkit",
+                        "segments": [],
+                        "ratios": [],
+                    },
+                ],
+                unfiltered_cnvs=[
+                    # File with amplifications
+                    {
+                        "chr1": {
+                            "cnvkit": [
+                                CNV(
+                                    caller="cnvkit",
+                                    chromosome="chr1",
+                                    genes=["gene1"],
+                                    start=100,
+                                    length=100,
+                                    type="DUP",
+                                    copy_number=6,
+                                    baf=0.3,
+                                ),
+                                CNV(
+                                    caller="cnvkit",
+                                    chromosome="chr1",
+                                    genes=["gene2"],
+                                    start=500,
+                                    length=100,
+                                    type="COPY_NORMAL",
+                                    copy_number=2,
+                                    baf=0.5,
+                                ),
+                            ],
+                        },
+                    },
+                    # File with deletions
+                    {
+                        "chr1": {
+                            "cnvkit": [
+                                CNV(
+                                    caller="cnvkit",
+                                    chromosome="chr1",
+                                    genes=["gene1"],
+                                    start=200,
+                                    length=100,
+                                    type="COPY_NORMAL",
+                                    copy_number=1,
+                                    baf=0.5,
+                                ),
+                                CNV(
+                                    caller="cnvkit",
+                                    chromosome="chr1",
+                                    genes=["gene2"],
+                                    start=600,
+                                    length=100,
+                                    type="DEL",
+                                    copy_number=1,
+                                    baf=0.3,
+                                ),
+                            ],
+                        },
+                    },
+                ],
+                filtered_cnvs=[
+                    # Filtered amplifications
+                    {
+                        "chr1": {
+                            "cnvkit": [
+                                CNV(
+                                    caller="cnvkit",
+                                    chromosome="chr1",
+                                    genes=["gene1"],
+                                    start=100,
+                                    length=100,
+                                    type="DUP",
+                                    copy_number=6,
+                                    baf=0.3,
+                                ),
+                            ],
+                        },
+                    },
+                    # Filtered deletions
+                    {
+                        "chr1": {
+                            "cnvkit": [
+                                CNV(
+                                    caller="cnvkit",
+                                    chromosome="chr1",
+                                    genes=["gene2"],
+                                    start=600,
+                                    length=100,
+                                    type="DEL",
+                                    copy_number=1,
+                                    baf=0.3,
+                                ),
+                            ],
+                        },
+                    },
+                ],
+                expected_cnvs={
+                    "chr1": {
+                        "cnvkit": {
+                            # All four calls should end up in the results...
+                            "all_count": 4,
+                            # ... and two of them should pass the filtering
+                            "pass_filter_count": 2,
+                        }
+                    }
+                },
+            )
+        ]
+
+        for case in testcases:
+            d = merge_cnv_dicts(case.callers, [], [], [], case.chromosomes, case.filtered_cnvs, case.unfiltered_cnvs)
+
+            for chromosome in case.expected_cnvs:
+                chromosomedata = [x for x in d if x["chromosome"] == chromosome]
+                assert len(chromosomedata) == 1
+
+                for caller in case.expected_cnvs[chromosome]:
+                    callerdata = [x for x in chromosomedata[0]["callers"] if x["name"] == caller]
+                    assert len(callerdata) == 1
+
+                    assert case.expected_cnvs[chromosome][caller]["all_count"] == len(callerdata[0]["cnvs"])
+
+                    n_pass_filter = sum(x["passed_filter"] for x in callerdata[0]["cnvs"])
+                    assert n_pass_filter == case.expected_cnvs[chromosome][caller]["pass_filter_count"]
