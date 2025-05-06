3c46481c7b8b75196af861a188f5c1c5c0e5f663
Niklas Mähler <niklas.mahler@gmail.com>
2025-05-06 18:18:23 +0200

    Merge gene lists and filter status for duplicate CNVs

--- workflow/scripts/merge_cnv_json.py
+++ workflow/scripts/merge_cnv_json.py
@@ -221,6 +221,13 @@ def merge_cnv_calls(unfiltered_cnvs, filtered_cnvs):
                 for c in m_cnvs:
                     if c not in cnvs:
                         cnvs.append(c)
+                    else:
+                        # If it already exists, make sure that it represents
+                        # all genes and that the filtering status is updated
+                        added_cnv = cnvs.pop(cnvs.index(c))
+                        added_cnv.genes = list(set(added_cnv.genes + c.genes))
+                        added_cnv.passed_filter = added_cnv.passed_filter or c.passed_filter
+                        cnvs.append(added_cnv)
     return sort_cnvs(cnvs)
 
 
